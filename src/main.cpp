d5596fc81d881c79cdf619ef45cc4f7191d46f2a
Carl Findahl <carl@findahl.net>
2018-02-17 11:03:29 +0100

    Fix #16 #17 Create default CMakeLists and Add Dependencies
    Added Following dependencies:
     - SFML
     - ImGUI
     - ImGUI SFML
     - OpenGL
     - FMT
    
    All dependencies are managed through CMake. Created custom CMakeLists
    for ImGUI since nothing was included. Used SFML one from interwebs,
    although the search paths must probably be modified for Windows,
    but we can deal with that when required. Added a main.cpp that currently
    tests and uses at least one thing from each dependency. It compiles
    fine. Uses basic example from SFML.com with a green circle.

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include "fmt/format.h"
+#include "imgui-SFML.h"
+#include "imgui.h"
+#include "json.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// Main tests that every library works!
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), fmt::format("Title{}", 4));
+
+	ImGui::SFML::Init(window);
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	nlohmann::json hello;
+	hello["Main"] = 42;
+
+	std::cout << hello;
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
+			if (event.type == sf::Event::Closed) window.close();
+		}
+
+		ImGui::SFML::Update(window, sf::Time(sf::seconds(1.f/60.f)));
+
+		ImGui::Begin("Window Title");
+		ImGui::Button("HELLO I WORK");
+		ImGui::End();
+
+		window.clear();
+		window.draw(shape);
+		ImGui::SFML::Render(window);
+		window.display();
+	}
+
+	ImGui::SFML::Shutdown();
+
+	return 0;
+}
