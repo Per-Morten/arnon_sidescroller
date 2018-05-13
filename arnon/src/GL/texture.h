9a5fcdda172ec9230868a8280787244f326e6f9b
Carl Findahl <sportleroro@gmail.com>
2018-05-13 22:53:46 +0200

    Create shell for Texture class
    Preparing for implementation

---
+++ arnon/src/GL/texture.h
@@ -0,0 +1,20 @@
+/// ArnonSidescroller
+
+/*
+* For loading and binding Textures to the OpenGL context.
+*/
+
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+
+class Texture
+{
+private:
+
+public:
+
+};
+
+
+#endif // TEXTURE_H
