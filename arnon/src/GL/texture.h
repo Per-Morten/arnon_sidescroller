dee1ac0783a87d897ae3497d32a9acf9faa7e806
Carl Findahl <sportleroro@gmail.com>
2018-05-14 07:53:58 +0200

    Create initial texture abstraction
    The texture does not support mipmaps and only loads from a single file. It can be reloaded after use, which will create a new texture object. With move ctor. Copy Ctor will come.

--- arnon/src/GL/texture.h
+++ arnon/src/GL/texture.h
@@ -1,20 +1,52 @@
 /// ArnonSidescroller
 
 /*
-* For loading and binding Textures to the OpenGL context.
-*/
+ * For loading and binding Textures to the OpenGL context.
+ */
 
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include <cstdint>
+#include <string>
 
 class Texture
 {
 private:
+    // The OpenGL Texture Name
+    uint32_t m_name = 0;
 
 public:
+    // Default ctor
+    Texture() = default;
+
+    // Construct from filepath
+    Texture(const std::string& filepath);
+    
+    // #TODO : Texture(const std::filesystem::path& filepath);
+
+    // Move Ctor
+    Texture(Texture&& other);
+
+    // Move Ass
+    Texture& operator=(Texture&& other);
+
+    // Dtor
+    ~Texture();
+
+    // Bind to given bindingPoint
+    void bind(const unsigned bindingPoint);
+
+    // Unbind from given binding point
+    void unbind(const unsigned bindingPoint);
+
+    // Load an image file into this texture
+    void loadFromFile(const std::string& filepath);
+
+private:
+    // Check if the texture is a valid OpenGL object
+    const bool isValid() const;
 
 };
-
 
 #endif // TEXTURE_H
