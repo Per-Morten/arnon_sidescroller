3215f84111e16035a1ed31799d2ad72947e68dfe
Carl Findahl <sportleroro@gmail.com>
2018-06-30 10:03:32 +0200

    Add state file before leaving to Australia

---
+++ arnon/src/sceneState.h
@@ -0,0 +1,16 @@
+/// ArnonSidescroller
+
+#ifndef SCENESTATE_H
+#define SCENESTATE_H
+
+struct PersistentSceneState
+{
+    // Info that should be generally Available
+};
+
+struct SceneState
+{
+    // Info that is specific but relevant to a Scene
+};
+
+#endif // SCENESTATE_H
